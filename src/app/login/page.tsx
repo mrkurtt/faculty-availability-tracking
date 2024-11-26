2c8f22d227fa4c4d2ca5b800bd706be753843255
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-11-26 16:20:32 +0800

    chore: changed application name to factrac

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,9 +6,7 @@ const Page = () => {
 		<div className="w-full h-screen flex justify-center items-center">
 			<Card className="w-96">
 				<CardBody className="p-6 text-center">
-					<h1 className="text-blue-500 font-semibold text-xl">
-						Faculty Availability Tracking Admin
-					</h1>
+					<h1 className="text-blue-500 font-semibold text-xl">factrac</h1>
 					<p className="">LOGIN</p>
 
 					<div className="flex flex-col gap-y-4 mt-8">
